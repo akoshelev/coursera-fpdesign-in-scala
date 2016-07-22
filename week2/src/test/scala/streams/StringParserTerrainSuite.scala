c8e002cc6392622b1214913c75f8ead2902c37e9
Alex Koshelev <alex.koshelev@lgm.ca>
2016-07-21 17:14:21 -0700

    adds week2 assignment (no solution yet)

---
+++ week2/src/test/scala/streams/StringParserTerrainSuite.scala
@@ -0,0 +1,10 @@
+package streams
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class StringParserTerrainSuite extends FunSuite {
+
+}
